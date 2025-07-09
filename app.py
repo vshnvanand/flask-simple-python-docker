1c61832b06bd9b0235eb4bbf90ffdca5c90a98e3
devVaishnav <vaishnavanand541@gmail.com>
2025-07-09 14:54:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Dosto, Kya Haal chall'
+    return 'Hello Dosto, Kya haal chaal kaise ho sab theek?"
 
 @app.route('/health')
 def health():
